2a575c46d14c2923ef871761fe7213dcbe57f36d
chengricui <chengri1105@gmail.com>
2018-08-09 17:44:53 +0300

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.2;
+
+contract DappToken {
+    // Constructor
+    //Set the total number of tokens
+    // Read the totla number of tokens
+    uint256 public totalSupply;
+    
+    function DappToken () public {
+        totalSupply = 1000000;
+
+    }
+
+}
