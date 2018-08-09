d55ff21d563a20f0595df74741e003b53ec91a51
chengricui <chengri1105@gmail.com>
2018-08-09 22:53:20 +0300

    3. delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -10,10 +10,17 @@ contract DappToken {
     uint256 public totalSupply;
     
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -35,6 +42,30 @@ contract DappToken {
         return true;
 
     }
+
+    //approve
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        allowance[msg.sender][_spender] = _value;
+
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    //transferfrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
+
 
 
 
